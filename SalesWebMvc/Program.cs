36b877fd048dc7777ae02628ca09efcba220e647
Gabriel <gabrielsprata@hotmail.com>
2023-07-26 22:03:58 -0300

    [FEAT] MySql Adaptation and first migration

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using SalesWebMvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+// Configuração
+builder.Configuration.AddJsonFile("appsettings.json");
+
+// Configuração do banco de dados
+builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+    options.UseMySql(builder.Configuration["ConnectionStrings:SalesWebMvcContext"], ServerVersion.Parse("8.0.34-mysql")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
